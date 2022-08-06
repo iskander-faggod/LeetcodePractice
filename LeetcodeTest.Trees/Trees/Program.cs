17b9d4286c5b53336e25be4be878084176be770a
iskander-faggod <kudashev.iskander13@gmail.com>
2022-08-06 13:16:37 +0300

    feat: trees test

---
+++ LeetcodeTest.Trees/Trees/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LeetcodeTest.Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leftNode = new TreeNode()
+            {
+                left = null,
+                right = null,
+                val = 9
+            };
+
+            var rightNode = new TreeNode()
+            {
+                left = new TreeNode()
+                {
+                    left = null,
+                    right = null,
+                    val = 15
+                },
+                right = new TreeNode()
+                {
+                    left = null,
+                    right = null,
+                    val = 7
+                },
+                val = 20
+            };
+            TreeNode root = new TreeNode()
+            {
+                left = leftNode,
+                right = rightNode,
+                val = 3,
+            };
+            InvertTree(root);
+            MaxDepth(root);
+        }
+
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root is null) return null;
+            (root.left, root.right) = (root.right, root.left);
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var left = MaxDepth(root.left);
+            var right = MaxDepth(root.right);
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
